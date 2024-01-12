a1669e0d48e8dd6b4dbae9350070a42c992a6c61
Semenov Ilya <nipacon@gmail.com>
2024-01-12 16:04:24 +0300

    menuExtra

--- widgets/menuExtra.php
+++ widgets/menuExtra.php
@@ -16,48 +16,33 @@ class menuExtra extends \yii\bootstrap4\Widget
         echo '
             <!-- extra Menu -->
             <nav id="extra-menu" class="site-navigation" role="navigation">
-                <div class="menu-themedefault-container ">
-                    <ul class="nav-menu sf-js-enabled">
-                        <li><a href="/">Home</a></li>
-                        <li class="page_item page-item-1779"><a href="/site/contact">Contact
-                                Us</a></li>
-                        <li class="page_item page-item-5 page_item_has_children"><a
-                                    href="#" class="sf-with-ul">Taxi lists<span class="sf-sub-indicator"> »</span></a>
-                            <ul class="children" style="display: none; visibility: hidden;">
-                                <li class="page_item page-item-1133"><a
-                                            href="/site/driver">Driver</a>
-                                </li>
-                                <li class="page_item page-item-1134"><a
-                                            href="/site/order">Order</a></li>
-                            </ul>
-                        </li>
-                        <li class="page_item page-item-146"><a href="/catalog/auto">Catalog</a>
-                        </li>
-                    </ul>
+                <div class="menu-themedefault-container ">';
+                    echo Nav::widget([
+                        'options' => ['class' => 'nav-menu sf-js-enabled'],
+                        'items' => [
+                            ['label' => 'Home', 'url' => ['/site/index']],
+                            ['label' => 'About', 'url' => ['/site/about']],
+                            ['label' => 'Contact Us', 'url' => ['/site/contact']],
+                            ['label' => 'Catalog', 'url' => ['/catalog/auto']],
+
+                            Yii::$app->user->isGuest ? (
+                            ['label' => 'Login', 'url' => ['/user/login']]
+                            ) : (
+                                '<li class="nav-item">'
+                                . Html::beginForm(['/user/logout'], 'post', ['class' => 'form-inline'])
+                                . Html::submitButton(
+                                    'Logout (' . Yii::$app->user->identity->username . ')',
+                                    ['class' => 'btn btn-link logout']
+                                )
+                                . Html::endForm()
+                                . '</li>'
+                            )
+                        ],
+                    ]);
+            echo '
                 </div>
             </nav>
             <!-- / extra Menu -->
         ';
-
-        Nav::widget([
-            'options' => ['class' => 'navbar-nav'],
-            'items' => [
-                ['label' => 'Home', 'url' => ['/site/index']],
-                ['label' => 'About', 'url' => ['/site/about']],
-                ['label' => 'Contact', 'url' => ['/site/contact']],
-                Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/user/login']]
-                ) : (
-                    '<li>'
-                    . Html::beginForm(['/user/logout'], 'post', ['class' => 'form-inline'])
-                    . Html::submitButton(
-                        'Logout (' . Yii::$app->user->identity->username . ')',
-                        ['class' => 'btn btn-link logout']
-                    )
-                    . Html::endForm()
-                    . '</li>'
-                )
-            ],
-        ]);
     }
 }
