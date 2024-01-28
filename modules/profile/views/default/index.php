cad035ad1b3da0be44c5fd9b4048239713ef2dd4
Semenov Ilya <nipacon@gmail.com>
2024-01-28 22:36:57 +0300

    edit profile

--- modules/profile/views/default/index.php
+++ modules/profile/views/default/index.php
@@ -1,9 +1,15 @@
 <div class="profile-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
+    <h1><?= Yii::t('app', 'Cabinet'); ?></h1>
     <div class="row">
         <div class="col-sm-2">
             <?php echo \app\widgets\menuProfile::widget() ?>
         </div>
-        <div class="col-sm-10">.col-sm-10</div>
+        <div class="col-sm-10">
+            <?php
+            echo '<pre>';
+            \yii\helpers\VarDumper::dump(Yii::$app->user->identity->attributes, 3, false);
+            echo '</pre>';
+            ?>
+        </div>
     </div>
 </div>
