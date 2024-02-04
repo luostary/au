5034a29fa410d685e55f869b53f87f351faa4ff4
Semenov Ilya <nipacon@gmail.com>
2024-02-04 15:01:14 +0300

    selectCarMark.php widget

---
+++ widgets/selectCarMark.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\widgets;
+
+use app\modules\autobasebuy\models\CarMark;
+use yii\base\Widget;
+
+class selectCarMark extends Widget
+{
+    public $form;
+    public $model;
+
+    public function run()
+    {
+        return $this->form->field($this->model, 'id_car_mark')->widget(\kartik\select2\Select2::class, [
+                'data' => CarMark::listAll(),
+                'options' => ['prompt' => ''],
+                'pluginOptions' => [
+                    'allowClear' => true,
+                    'placeholder' => 'Не указано'
+                ],
+                'pluginEvents' => [
+                    "change" => "function(a) {
+                    $.ajax({
+                        url: '/catalog/auto/get-model-list',
+                        data: {
+                            'id_car_mark': $(this).val(),
+                        },
+                        success: function (data) {
+                            $('#car-id_car_model').html(data);
+                        }
+                    });
+                }",
+                ],
+            ]
+        );
+    }
+}
