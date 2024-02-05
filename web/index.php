c23bafe587f1db5ede020040861bf3e77f5dda9d
Semenov Ilya <nipacon@gmail.com>
2024-02-05 13:16:30 +0300

    composer checking

--- web/index.php
+++ web/index.php
@@ -4,7 +4,11 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require __DIR__ . '/../vendor/autoload.php';
+$autoload = __DIR__ . '/../vendor/autoload.php';
+if (!file_exists($autoload)) {
+    die('Установите composer и запустите команду composer install');
+}
+require $autoload;
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
