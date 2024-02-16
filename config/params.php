2fe20f5dbbd24a069e512843024dd293a79fbffc
Semenov Ilya <nipacon@gmail.com>
2024-02-16 10:04:30 +0300

    local params.php

--- config/params.php
+++ config/params.php
@@ -1,7 +1,13 @@
 <?php
 
-return [
+$params = [
     'adminEmail' => 'guapac@yandex.ru',
     'senderEmail' => 'guapac@yandex.ru',
     'senderName' => 'Autohub',
 ];
+
+if (file_exists(__DIR__ . '/private/params-local.php')) {
+    $params = require __DIR__ . '/private/params-local.php';
+}
+
+return $params;
